dde20a0695c51976d657c379f43ffa9accdaec9c
DYung26 <theadstore1407@gmail.com>
2024-10-04 00:34:00 +0100

    Updated the project title from `dMarqt` to `projectX` in the layout

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -36,7 +36,7 @@ export default function RootLayout({ children }) {
 
                 <meta charset="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <title>dMarqt</title>
+                <title>projectX</title>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link
                     rel="preconnect"
