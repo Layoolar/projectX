b66aa0aad2c92e351d6c2b06f69ecf217dedd567
DYung26 <theadstore1407@gmail.com>
2024-10-17 07:22:42 +0100

    Refactor task verification logic and task processing

--- backend/src/infrastructure/Scheduler/TaskCronJob.ts
+++ backend/src/infrastructure/Scheduler/TaskCronJob.ts
@@ -1,18 +1,19 @@
-import { UserTaskStatus } from '@domain/models';
-import { handleTaskResult } from '@infrastructure/twitter/utils';
+import { UserTask, UserTaskStatus } from '@domain/models';
+import { TwitterService } from '@infrastructure/twitter/TwitterService';
 import { extractId, makeTwitterRequestWithTokenRefresh, verifyRetweet, verifyTweetLike, verifyUserFollow } from '@utils/twitterapi';
+import { getCurrentTimeStamp } from '@utils/utilsCommon';
 import { CustomError } from 'application/errors';
-import { taskDBRepo, userDBRepo, userTaskDBRepo, UserTaskRepositoryImpl } from 'application/repoimpls';
+import { taskDBRepo, TaskRepositoryImpl, userDBRepo, UserRepositoryImpl, userTaskDBRepo, UserTaskRepositoryImpl } from 'application/repoimpls';
 import { StatusCodes } from 'http-status-codes';
 import cron from 'node-cron';
 
-const taskThreshold = 100;
+const taskThreshold = 0;
 
-const actionMap: { [key: string]: any } = {
-    like: verifyTweetLike,
-    follow: verifyUserFollow,
-    retweet: verifyRetweet,
-    comment: verifyRetweet,
+const actionMap: { [key: string]: (tweetId: string) => Promise<string[]> } = {
+    like: (tweetId: string) => new TwitterService().getTweetLikes(tweetId),
+    follow: (tweetId: string) => new TwitterService().getFollowers(tweetId),
+    comment: (tweetId: string) => new TwitterService().getTweetComments(tweetId),
+    retweet: (tweetId: string) => new TwitterService().getTweetRetweets(tweetId),
 };
 
 export const scheduleTaskProcessing = () => {
@@ -26,7 +27,7 @@ export const scheduleTaskProcessing = () => {
 	    if (queuedTaskCount >= taskThreshold) {
                 console.log(`Threshold reached with ${queuedTaskCount} tasks.
 			    Processing batch...`);
-		await processTasks(userTaskDBRepo);
+		await processTasks();
 	    } else {
                 console.log(`Only ${queuedTaskCount} tasks found. Waiting for more.`);
 	    }
@@ -36,13 +37,88 @@ export const scheduleTaskProcessing = () => {
     })
 }
 
-async function processTasks(
-    userTaskRepo: UserTaskRepositoryImpl,
+async function processUserTasksForAction(
+	userTasks: UserTask[], userIds: string[], userDBRepo: UserRepositoryImpl,
 ) {
-    try {
-	// let userTasksToUpdate = [];
-        const userTasksToProcess = await userTaskRepo.fetchQueuedTasks(taskThreshold);
+    return Promise.all(
+	userTasks.map(async (userTask) => {
+            const user = await userDBRepo.getUserById(userTask.userId);
+	    if (!user) {
+	        throw new CustomError(
+                    'User not found', StatusCodes.NOT_FOUND,
+	        );
+	    }
+            if (userIds.includes(user.twitter.id)) {
+                return userTask;
+	    }
+	    return null;
+	})
+    );
+}
 
+
+async function processTasks() {
+    try {
+	let userTasksToUpdate: UserTask[] = [];
+        const userTasksToProcess = await userTaskDBRepo.fetchQueuedTasks(taskThreshold);
+	const uniqueTaskIds = Array.from(new Set(userTasksToProcess.map(task => task.taskId)));
+
+        await Promise.all(
+            uniqueTaskIds.map(async (taskId) => {
+	        const task = await taskDBRepo.getTask(taskId);
+	        if (!task) throw new CustomError('Task not found', StatusCodes.NOT_FOUND);
+
+		const taskTweetId = extractId(task.url);
+	        if (!taskTweetId) throw new CustomError(
+			'Unable to extract id from task', StatusCodes.BAD_REQUEST
+		);
+
+	        const userTasksWithThisTaskId = userTasksToProcess.filter(
+	            userTask => userTask.taskId === taskId
+	        );
+		const actionVerifier = actionMap[task.action.toLowerCase()];
+	        
+		if (actionVerifier) {
+	            const userIds = await actionVerifier(taskTweetId);
+		    const validUserTasks = await processUserTasksForAction(
+			    userTasksWithThisTaskId, userIds, userDBRepo
+		    );
+
+		    validUserTasks
+                        .filter(Boolean)
+                        .forEach((validTask) => {
+                            if (validTask) {
+                                userTasksToUpdate.push(validTask);
+                            }
+                        });
+	        }
+	    })
+        );
+
+	await Promise.all(
+            userTasksToUpdate.map(async (userTask) => {
+                const userId = userTask.userId;
+                const task = await taskDBRepo.getTask(userTask.taskId);
+                if (!task) throw new CustomError('Task not found', StatusCodes.NOT_FOUND);
+
+                await userTaskDBRepo.createOrUpdateUserTask({
+                    userId, taskId: task.id, createdAt: getCurrentTimeStamp(),
+		    updatedAt: getCurrentTimeStamp(), status: UserTaskStatus.COMPLETED,
+                });
+
+                await userDBRepo.updateUserPoints(userId, task.reward);
+                await userDBRepo.updateReferrerReward(userId, task.reward);
+
+                console.log(`Updated task data for user ${userId} to completed`);
+            })
+        )
+    } catch (error) {
+        console.error('Error in processing tasks:', error);
+        throw error;
+    }
+}
+
+/*
         for (const userTask of userTasksToProcess) {
             console.log(`Processing task with ID: ${userTask.taskId}`);
 	    const userId = userTask.userId;
@@ -84,7 +160,7 @@ async function processTasks(
 	    }
         }
 
-        /*
+        
 	const userIds = userTasksToUpdate.map((ut) => ut.userId);
 	const taskIds = userTasksToUpdate.map((ut) => ut.taskId);
 	await userTaskRepo
@@ -92,11 +168,12 @@ async function processTasks(
 		taskIds,
 		userIds,
 		UserTaskStatus.COMPLETED);
-	*/
+	
 
         console.log('Batch processing completed.');
     } catch (error) {
         console.error('Error processing tasks:', error);
     }
 }
+*/
 
